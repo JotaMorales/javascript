9f0be2e7487f63e1c3fa3f5eca505869b0626f45
JotaMorales <jnmorales73@live.com>
2021-05-19 20:34:11 -0300

    desafio 2

--- js/desafio2.js
+++ js/desafio2.js
@@ -2,22 +2,30 @@ var respuesta = prompt('Quiere registrarse?')
 console.log(respuesta);
 if (respuesta == 'si') {
     alert('Genial')
-    var respuestaPositiva = prompt('Ingrese su nombre de usuario')
+    var respuestaPositiva = prompt('Ingrese su nombre de usuario:')
     console.log(respuestaPositiva);
-    var contrasena = prompt('Ingrese su contraseña')
+    var contrasena = prompt('Ingrese su contraseña:')
     console.log(contrasena);
     alert('Bienvenido');
 } else if (respuesta == 'no') {
-    alert('Un placer');
+    alert('Hasta la próxima');
 }
 
+
+
+
 var respuesta = prompt('Quieres mirar esta pelicula?')
-var edad = parseInt(prompt('Ingresa tu edad'))
-if ((respuesta == 'si') && (edad >= 18)) {
-    console.log(respuesta);
-    alert('Disfruta la pelicula');
-} else if ((respuesta == 'si') && (edad <= 18)) {
-    alert('Lo siento no tienes la edad suficiente');
-} else if (respuesta == 'no') {
-    alert('Tu te lo pierdes')
+if (respuesta == 'no') {
+    alert('Tu te lo pierdes');
+} else {
+    var edad = parseInt(prompt('Ingresa tu edad:'))
+    if ((respuesta == 'si') && (edad >= 18)) {
+        console.log(respuesta);
+        alert('Disfruta la pelicula');
+    } else if ((respuesta == 'si') && (edad <= 18)) {
+        alert('Lo siento no tienes la edad suficiente');
+    } else {
+        alert('Ingresá una respuesta válida');
+    }
+
 }
