f716db41d009f5a35fd7d360621b21f0752a252d
JotaMorales <jnmorales73@live.com>
2021-06-07 00:21:46 -0300

    nueva correccion

---
+++ desafio4/index.js
@@ -0,0 +1,11 @@
+alert('Calculemos el IVA en tus productos seleccionados');
+
+function iva(precio) {
+    const IVA = 0.21;
+    return (precio * IVA) + precio;
+};
+
+const precioCosto = parseInt(prompt('Ingrese el precio del producto'));
+const precioCalculado = iva(precioCosto);
+
+alert('El precio del producto con IVA: ' + precioCalculado);
